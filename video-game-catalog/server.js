6e2f6a0aead90336bd02915b8185f0af7d34fba8
saad <saadsalahuddin21@gmail.com>
2025-08-12 20:44:50 +0530

    this is testing

--- video-game-catalog/server.js
+++ video-game-catalog/server.js
@@ -6,18 +6,15 @@ const gameRoutes = require('./routes/gameRoutes');
 
 const app = express();
 
-// Middleware to parse JSON and add timestamp
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTimestamp = new Date().toISOString();
   next();
 });
 
-// Routes
 app.use('/api/publishers', publisherRoutes);
 app.use('/api/games', gameRoutes);
 
-// Connect to MongoDB and start server
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
